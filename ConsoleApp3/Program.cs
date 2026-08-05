b6dcdda0f7d25d5f570c12d065a3ffd2b5c850e9
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-08-06 00:22:26 +0300

    Question01

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ConsoleApp3
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            //1- Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75.Print the second price(index 1).
+
+            double[] prices = { 25.5, 40.0, 33.75 };
+
+            Console.WriteLine(prices[1]);
+            #endregion
         }
     }
 }
