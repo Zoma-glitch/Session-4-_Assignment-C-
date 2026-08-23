ecd1c5b706ec0780bbc242d98c8e36bc6ce1709a
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-08-23 17:29:47 +0300

    Question2

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -13,14 +13,32 @@ namespace ConsoleApp3
 
             //Console.WriteLine(prices[1]);
             #endregion
-            PrintWelcomeMessage();
-        }
-        #region Question02
+            //PrintWelcomeMessage();
 
-        private static void PrintWelcomeMessage()
-        {
-            Console.WriteLine("Welcome to the Library!");
+
+            #region Question2
+
+            //2- Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+
+            int[,] shelfCopies =
+                {
+
+            {3,5},
+            {1,4}
+
+            };
+
+            Console.WriteLine(shelfCopies[1, 0]);
+
+
+            #endregion
         }
+        #region Question02  
+
+        //private static void PrintWelcomeMessage()
+        //{
+        //    Console.WriteLine("Welcome to the Library!");
+        //}
         #endregion
     }
 }
